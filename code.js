3989dd65de12bf7403bbd9805453e0c30c834466
kenzieadmin <kenzieadmin@users.noreply.github.com>
2023-04-18 16:31:53 +0000

    deploy: 0ca63a059484d45f7a9b585107a5a5cd067cec0c

--- code.js
+++ code.js
@@ -10,41 +10,16 @@ let todos = [
 let renderTodoApp = function () {
   // Follow the instructions on my.kenzie.academy to complete the assignment.
   // Your Code Here
-  // Step One 
-  let app = document.getElementById('app')
-  let newElement = document.createElement('h3')
-  newElement.innerText = 'Todo List'
-  app.append(newElement)
-
-  // Step Two
-  let unordList = document.createElement('ul')
+  let app = '<div id="app"><h3>Todo List</h3>'
+  
+  let unordList = '<ul>'
   for (let item of todos) {
-    let list = document.createElement('li')
-    list.classList.add('list-item')
-    list.innerText = item
-    unordList.append(list)
-
-    //Step Three begining
-    //style the items list
-    list.style.width = '200px'
-    list.style.height = '200px'
-    list.style.backgroundColor = 'lightgray'
-    list.style.margin = '10px 30px'
-    list.style.padding = '10px'
-    list.style.listStyle = 'none'
-
+    // let list = document.createElement('li')
+    unordList += `<li class = "list-item">${item}</li>`
   }
-  app.append(unordList)
-
-    //Step Three Ends
-    //center the title 
-  newElement.style.textAlign= 'center'
-
-  //organize the items list using their parentNode
-  unordList.style.display= "flex"
-  unordList.style.flexWrap= 'wrap'
-  unordList.style.margin= '0 200px'
-  unordList.style.paddingLeft= '55px'
+  unordList += '</ul>'
+  app += unordList + '</div>'
+  document.write(app)
 }
 
 renderTodoApp();
