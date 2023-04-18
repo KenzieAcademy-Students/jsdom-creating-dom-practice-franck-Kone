c58229bb0ce1ee8173b80ea2963e282d5929cf01
franck <franck.kone@mykenzie.snhu.edu>
2023-04-17 21:53:54 -0400

    completed step one and two activity creating DOM

--- code.js
+++ code.js
@@ -10,6 +10,22 @@ let todos = [
 let renderTodoApp = function() {
   // Follow the instructions on my.kenzie.academy to complete the assignment.
   // Your Code Here
+  // Step One 
+let app = document.getElementById('app')
+let newElement = document.createElement('h3')
+newElement.innerText = 'Todo List'
+app.append(newElement)
+console.log(app)
+
+  // Step Two
+  let unordList = document.createElement('ul')
+  for(let item of todos){
+    let list = document.createElement('li')
+    list.classList.add('list-item')
+    list.innerText = item
+    unordList.append(list)
+  }
+  app.append(unordList)
 
  
 }
